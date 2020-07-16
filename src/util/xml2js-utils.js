7f5b66ea2817edacdc163e01443dcd68a9ebf62a
Kyle Karpack <kylekarpack@gmail.com>
2020-07-15 20:37:56 -0700

    Fix XML parsing

--- src/util/xml2js-utils.js
+++ src/util/xml2js-utils.js
@@ -1,67 +1,80 @@
-class Xml2JsUtils {
+export class Xml {
 
-	static nativeType(value) {
-		let nValue = Number(value);
-		if (!isNaN(nValue)) {
-			return nValue;
-		}
-		if (typeof value === "string") {
-			let bValue = value.toLowerCase();
-			if (bValue === "true") {
-				return true;
-			} else if (bValue === "false") {
-				return false;
-			}
-		}		
-		return value;
+	static parseXmlStringToObject = (stringContent) => {
+		const parsed = Xml.parse(stringContent);
+		return Xml.xmlToJson(parsed);
 	};
 
-	static parse(xml) {
-		const parsed = new DOMParser().parseFromString(xml, "text/xml");
-		return Xml2JsUtils.xmlToJson(parsed);
-	}
+	static parse = (stringContent) => {
+		const parser = new DOMParser();
+		return parser.parseFromString(stringContent, "text/xml");
+	};
 
-	static xmlToJson(xml) {
-
+	static xmlToJson = (xml) => {
 		// Create the return object
 		let obj = {};
 
-		if (xml.nodeType === 1) { // element
-			// do attributes
+		/*
+		if (xml.nodeType === 1) {
+			// element
+			// do attributes (not needed for BeerXml?)
 			if (xml.attributes.length > 0) {
 				obj["@attributes"] = {};
 				for (let j = 0; j < xml.attributes.length; j++) {
 					const attribute = xml.attributes.item(j);
-					obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+					obj["@attributes"][camelCase(attribute.nodeName)] =
+						attribute.nodeValue;
 				}
 			}
-		} else if (xml.nodeType === 3) { // text
+		} */
+		if (xml.nodeType === 3) {
+			// text
 			obj = xml.nodeValue;
 		}
 
 		// do children
-		// If just one text node inside
-		if (xml.hasChildNodes() && xml.childNodes.length === 1 && xml.childNodes[0].nodeType === 3) {
-			obj = Xml2JsUtils.nativeType(xml.childNodes[0].nodeValue);
-		}
-		else if (xml.hasChildNodes()) {
+		if (xml.hasChildNodes()) {
 			for (let i = 0; i < xml.childNodes.length; i++) {
 				const item = xml.childNodes.item(i);
 				const nodeName = item.nodeName;
-				if (typeof (obj[nodeName]) == "undefined") {
-					obj[nodeName] = Xml2JsUtils.xmlToJson(item);
+				if (typeof obj[nodeName] === "undefined") {
+					obj[nodeName] = Xml.xmlToJson(item);
 				} else {
-					if (typeof (obj[nodeName].push) == "undefined") {
+					if (typeof obj[nodeName].push === "undefined") {
 						const old = obj[nodeName];
 						obj[nodeName] = [];
 						obj[nodeName].push(old);
 					}
-					obj[nodeName].push(Xml2JsUtils.xmlToJson(item));
+					obj[nodeName].push(Xml.xmlToJson(item));
 				}
 			}
 		}
-		return obj;
-	}
-}
 
-export default Xml2JsUtils;
+		// Move up all text attrs
+		for (let key in obj) {
+			if (typeof obj[key]["#text"] === "string") {
+				obj[key] = obj[key]["#text"];
+			} else if (typeof obj[key].text === "string") {
+				obj[key] = obj[key].text;
+			} else {
+				delete obj[key].text;
+				delete obj[key]["#text"];
+			}
+		}
+
+		// Move up all sub-arrays
+		for (let key in obj) {
+			const value = obj[key];
+			const keys = Object.keys(value);
+			if (
+				typeof value === "object" &&
+				keys.length === 1 &&
+				Array.isArray(value[keys[0]])
+			) {
+				obj[key] = value[keys[0]];
+			}
+		}
+
+		return obj;
+	};
+}
