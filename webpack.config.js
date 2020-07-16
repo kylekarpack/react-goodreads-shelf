e360fb2de976bb52491971ba2f3ff0fd14120458
Kyle Karpack <kylekarpack@gmail.com>
2020-07-16 06:49:41 -0700

    Add some improved minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,43 @@
-var path = require('path');
+const path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
 module.exports = {
-	entry: './src/index.js',
+	entry: "./src/index.js",
 	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'index.js',
-		library: "",      
-        libraryTarget: "commonjs"
+		path: path.resolve(__dirname, "dist"),
+		filename: "index.js",
+		library: "",
+		libraryTarget: "commonjs",
 	},
 	module: {
 		rules: [
 			{
 				test: /\.(js|jsx)$/,
-				include: path.resolve(__dirname, 'src'),
-				exclude: /(node_modules|bower_components|dist)/,
+				include: path.resolve(__dirname, "src"),
+				exclude: /(node_modules|bower_components|dist|.*\.spec\.js)/,
 				use: {
-					loader: 'babel-loader',
+					loader: "babel-loader",
 					options: {
-						presets: ['@babel/preset-env']
-					}
-				}
-			}
-		]
+						presets: ["@babel/preset-env"],
+					},
+				},
+			},
+		],
 	},
 	resolve: {
-		extensions: ['.js', '.jsx']
+		extensions: [".js", ".jsx"],
 	},
 	externals: {
-		react: {          
-            commonjs: "react",          
-            commonjs2: "react",          
-            amd: "React",          
-            root: "React"      
+		react: {
+			commonjs: "react",
+			commonjs2: "react",
+			amd: "React",
+			root: "React",
 		},
-		// "styled-components": {
-		// 	commonjs: "styled-components",
-		// 	commonjs2: "styled-components",
-		// 	amd: "styled-components",
-		// }
-	}
+	},
+	optimization: {
+		minimizer: [new UglifyJsPlugin({
+			cache: true
+		})],
+	},
 };
