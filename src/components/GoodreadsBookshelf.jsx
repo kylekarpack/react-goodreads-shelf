e9302dd331fbeb3912823f666f70e9bdec3c0c85
Kyle Karpack <kylekarpack@gmail.com>
2020-07-18 19:20:07 -0700

    Switch to functional bookshelf

--- src/components/GoodreadsBookshelf.jsx
+++ src/components/GoodreadsBookshelf.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Xml } from "../util/xml2js-utils";
 import Book from "./Book";
 import Loader from "./Loader";
@@ -12,41 +12,30 @@ const shelfStyle = (minWidth) => ({
 	textAlign: "center",
 });
 
-class GoodreadsBookshelf extends React.Component {
+export default (props) => {
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			books: [],
-			loaded: false,
-			options: props.options || { width: 100 }
-		};
-	}
+	const [state, setState] = useState({
+		books: [],
+		loaded: false,
+		options: props.options || { width: 100 },
+	});
 
-	componentWillMount() {
-		this.getBooks();
-	}
-
-	componentDidUpdate(prevProps) {
-		if (prevProps.limit !== this.props.limit) {
-			this.getBooks();
-		}
-	}
-
-	getUrl() {
+	const getUrl = () => {
 		// Build a request to the Goodreads API
-		const url = new URL(`https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/${this.props.userId}`);
-		url.searchParams.set("key", this.props.apiKey);
-		url.searchParams.set("per_page", this.props.limit || 10);
-		url.searchParams.set("shelf", this.props.shelf || "read");
-		url.searchParams.set("sort", this.props.sort || "date_read");
+		const url = new URL(
+			`https://cors-anywhere.herokuapp.com/https://www.goodreads.com/review/list/${props.userId}`
+		);
+		url.searchParams.set("key", props.apiKey);
+		url.searchParams.set("per_page", props.limit || 10);
+		url.searchParams.set("shelf", props.shelf || "read");
+		url.searchParams.set("sort", props.sort || "date_read");
 		url.searchParams.set("v", 2);
 		return url;
-	}
+	};
 
-	async getBooksJson() {
+	const getBooksJson = async () => {
 		if (typeof window !== "undefined" && window.fetch) {
-			const url = this.getUrl();
+			const url = getUrl();
 			const response = await fetch(url);
 			const xmlText = await response.text();
 			const json = Xml.parseXmlStringToObject(xmlText);
@@ -60,51 +49,57 @@ class GoodreadsBookshelf extends React.Component {
 			}
 
 			return books;
-
 		} else {
 			throw "Error: fetch is not defined in this environment";
 		}
-	}
+	};
 
-	async getBooks() {
-
-		this.setState({
-			loaded: false
+	const getBooks = async () => {
+		setState({
+			...state,
+			loaded: false,
 		});
 
 		try {
-			const books = await this.getBooksJson();
+			const books = await getBooksJson();
 
-			this.setState({
+			setState({
+				...state,
 				books: Array.isArray(books) ? books : [books],
-				loaded: true
+				loaded: true,
 			});
-
 		} catch (e) {
-
 			console.error(e);
 
 			// Indicate that we errored
-			this.setState({
+			setState({
+				...state,
 				loaded: true,
-				error: true
+				error: true,
 			});
 		}
-	}
+	};
 
-	render() {
-		return (
-			<div className="goodreads-shelf">
-				<div style={shelfStyle(this.state.options.width)}>
-					{this.state.books.map(book => {
-						return <Book key={book.id} book={book.book} options={this.state.options} />
-					})}
-				</div>
-				{ this.state.loaded ? "" : <Loader /> }
-				{ this.state.error ? <div>Sorry, we couldn't load books right now</div> : "" }
+	// Get books when props are updated
+	useEffect(() => {
+		getBooks();
+	}, [props])
+
+	return (
+		<div className="goodreads-shelf">
+			<div style={shelfStyle(state.options?.width)}>
+				{state.books.map((book) => {
+					return (
+						<Book key={book.id} book={book.book} options={state.options} />
+					);
+				})}
 			</div>
-		)
-	}
-}
-
-export default GoodreadsBookshelf;
+			{state.loaded ? "" : <Loader />}
+			{state.error ? (
+				<div>Sorry, we couldn't load books right now</div>
+			) : (
+				""
+			)}
+		</div>
+	);
+};
