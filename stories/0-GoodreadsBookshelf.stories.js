228642ae6b6de40b0cdf3d579bd87394bedaf413
Kyle Karpack <kylekarpack@gmail.com>
2020-07-18 19:58:09 -0700

    Update storybook

--- stories/0-GoodreadsBookshelf.stories.js
+++ stories/0-GoodreadsBookshelf.stories.js
@@ -1,4 +1,10 @@
-import { text, number, withKnobs } from "@storybook/addon-knobs";
+import {
+	boolean,
+	number,
+	text,
+	withKnobs,
+	select,
+} from "@storybook/addon-knobs";
 import React from "react";
 import { GoodreadsBookshelf } from "../dist/index";
 
@@ -7,10 +13,50 @@ export default {
 	decorators: [withKnobs],
 };
 
+const sorts = [
+	"title",
+	"author",
+	"cover",
+	"rating",
+	"year_pub",
+	"date_pub",
+	"date_pub_edition",
+	"date_started",
+	"date_read",
+	"date_updated",
+	"date_added",
+	"recommender",
+	"avg_rating",
+	"num_ratings",
+	"review",
+	"read_count",
+	"votes",
+	"random",
+	"comments",
+	"notes",
+	"isbn",
+	"isbn13",
+	"asin",
+	"num_pages",
+	"format",
+	"position",
+	"shelves",
+	"owned",
+	"date_purchased",
+	"purchase_location",
+	"condition",
+];
+
+const shelves = ["read", "currently-reading", "to-read"];
+
 export const Story = () => (
 	<GoodreadsBookshelf
-		userId={text("User ID", "63515611")}
-		apiKey={text("API Key", "PsmXJodsWJgBPgTosjdEkQ")}
-		limit={number("Limit", 20)}
+		userId={text("User ID", "63515611", "Auth")}
+		apiKey={text("API Key", "PsmXJodsWJgBPgTosjdEkQ", "Auth")}
+		width={number("Width", 100, { min: 20, max: 400 }, "Display")}
+		details={boolean("Show Details", false, "Display")}
+		limit={number("Limit", 20, { min: 1, max: 50 }, "API")}
+		shelf={select("Shelf", shelves, "read", "API")}
+		sort={select("Sort", sorts, "date_read", "API")}
 	/>
 );
