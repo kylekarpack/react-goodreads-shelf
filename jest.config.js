da5e6e30ed68bc3a46142d0bf612003dddf16d3f
Kyle Karpack <kylekarpack@gmail.com>
2020-08-20 20:44:43 -0700

    Test changes

--- jest.config.js
+++ jest.config.js
@@ -152,7 +152,7 @@ module.exports = {
 	// testRegex: [],
 
 	// This option allows the use of a custom results processor
-	// testResultsProcessor: null,
+	testResultsProcessor: "jest-sonar-reporter",
 
 	// This option allows use of a custom test runner
 	// testRunner: "jasmine2",
