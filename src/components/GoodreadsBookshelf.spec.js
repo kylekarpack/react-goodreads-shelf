4694e970a54266c7edd3d678a0c0f34e2b0baf5c
Kyle Karpack <kylekarpack@gmail.com>
2020-08-20 17:50:23 -0700

    Tests fixed

--- src/components/GoodreadsBookshelf.spec.js
+++ src/components/GoodreadsBookshelf.spec.js
@@ -1,29 +1,42 @@
 import GoodreadsBookshelf from "./GoodreadsBookshelf";
 import React from "react";
 import { shallow, mount, render } from "enzyme";
+import { act } from "react-dom/test-utils";
 
 describe("testing bookshelf", () => {
-
 	beforeEach(() => {
 		fetch.resetMocks();
-		fetch.mockResponseOnce(JSON.stringify({ GoodreadsResponse: { reviews: { review: [] } } }));
-	})
+		fetch.mockResponseOnce(
+			JSON.stringify({ GoodreadsResponse: { reviews: { review: [] } } })
+		);
+	});
 
 	it("renders without crashing", () => {
 		const shelf = shallow(<GoodreadsBookshelf />);
 		expect(shelf).toMatchSnapshot();
 	});
-	
-	it("passes props properly", () => {
-		const shelf = shallow(<GoodreadsBookshelf apiKey="test" userId="testUser" limit={15} shelf="read" sort="date_read" />);
-		const props = shelf.instance().props;
-		expect(props.limit).toEqual(15);
-		expect(props.userId).toEqual("testUser");
-		expect(props.apiKey).toEqual("test");
-		expect(props.shelf).toEqual("read");
-		expect(props.sort).toEqual("date_read");
+
+	it("passes props properly", async () => {
+		await act(async () => {
+			const shelf = mount(
+				<GoodreadsBookshelf
+					apiKey="test"
+					userId="testUser"
+					limit={15}
+					shelf="read"
+					sort="date_read"
+				/>
+			);
+
+			const props = shelf.props();
+			expect(props.limit).toEqual(15);
+			expect(props.userId).toEqual("testUser");
+			expect(props.apiKey).toEqual("test");
+			expect(props.shelf).toEqual("read");
+			expect(props.sort).toEqual("date_read");
+		});
 	});
-	
+
 	// it("gets books", async () => {
 	// 	const shelf = shallow(<GoodreadsBookshelf apiKey="test" userId="testUser" limit={15} shelf="read" sort="date_read" />);
 	// 	const books = await shelf.instance().getBooksJson();
