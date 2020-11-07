2e0bae877fc8c9dd247575f32ca3db6cb7f63158
Kyle Karpack <kylekarpack@gmail.com>
2020-11-06 18:11:11 -0800

    Add controls

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,6 +4,7 @@ module.exports = {
 		"@storybook/addon-actions",
 		"@storybook/addon-links",
 		"@storybook/addon-storysource",
+		"@storybook/addon-controls"
 	],
 	webpackFinal: async (config) => {
 		// do mutation to the config
