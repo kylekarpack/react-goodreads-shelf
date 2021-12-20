c83cdfa4582af2e0c10a4ae681c110baaf255b72
Kyle Karpack <kylekarpack@gmail.com>
2021-12-20 11:50:48 -0800

    Additional test updates

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,13 @@
 // For a detailed explanation regarding each configuration property, visit:
 // https://jestjs.io/docs/en/configuration.html
-
-const swcConfigs = {
-  module: {
-    type: "commonjs"
-  }
-};
-
 module.exports = {
   automock: false,
   coverageDirectory: "coverage",
-  collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
+  collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!**/src/index.ts", "!**/src/types/**", "!**/node_modules/**"],
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
   transform: {
-    "^.+\\.(t|j)sx?$": ["@swc/jest"],
+    "^.+\\.(t|j)sx?$": "@swc/jest",
     "^.+\\.svg$": "jest-svg-transformer"
   },
   transformIgnorePatterns: ["/node_modules/"],
