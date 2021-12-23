37a412cbde29a8ce2a07e9be7b326a7da18417f0
Kyle Karpack <kylekarpack@gmail.com>
2021-12-22 18:16:25 -0800

    Refactor a few areas

--- src/hooks/useGoodreadsShelf.ts
+++ src/hooks/useGoodreadsShelf.ts
@@ -1,11 +1,32 @@
 import { useEffect, useState } from "react";
-import { Props } from "../types";
+import { Book, Props } from "../types";
 import { getUrl } from "../util/get-url";
 
+const bookMapper = (row: Element, index: number): Book => {
+  const isbn = row?.querySelector("td.field.isbn .value")?.textContent?.trim();
+  const title = row?.querySelector("td.field.title a")?.getAttribute("title") ?? "";
+  const author = row?.querySelector("td.field.author .value")?.textContent?.trim();
+  const image_url = row
+    ?.querySelector("td.field.cover img")
+    ?.getAttribute("src")
+    // Get the full sized thumbnail
+    ?.replace(/\._\w+\d+_/, "");
+  const href = row?.querySelector("td.field.cover a")?.getAttribute("href");
+
+  return {
+    id: `${isbn}_${index}`,
+    isbn,
+    title,
+    author,
+    image_url,
+    link: `https://www.goodreads.com/${href}`
+  };
+};
+
 export default function useGoodreadsShelf(props: Props) {
-  const [books, setBooks] = useState<any[]>([]);
+  const [books, setBooks] = useState<Book[]>([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<any>(null);
+  const [error, setError] = useState<unknown>(null);
 
   useEffect(() => {
     const fetchBooks = async () => {
@@ -18,27 +39,13 @@ export default function useGoodreadsShelf(props: Props) {
         const goodreadsDocument = parser.parseFromString(await response.text(), "text/html");
         const bookElements = Array.from(goodreadsDocument.querySelectorAll("#booksBody .bookalike")).slice(
           0,
-          props.limit || 10
+          props.limit ?? 10
         );
 
-        const books = bookElements.map((row, index) => {
-          const isbn = row?.querySelector("td.field.isbn .value")?.textContent?.trim();
-          return {
-            id: `${isbn}_${index}`,
-            isbn: isbn,
-            title: row?.querySelector("td.field.title a")?.getAttribute("title"),
-            author: row?.querySelector("td.field.author .value")?.textContent?.trim(),
-            image_url: row
-              ?.querySelector("td.field.cover img")
-              ?.getAttribute("src")
-              // Get the full sized thumbnail
-              ?.replace(/\._\w+\d+_/, ""),
-            link: `https://www.goodreads.com/${row?.querySelector("td.field.cover a")?.getAttribute("href")}`
-          };
-        });
+        const books = bookElements.map(bookMapper);
 
         setBooks(books);
-      } catch (error) {
+      } catch (error: unknown) {
         setError(error);
       } finally {
         setLoading(false);
---
+++ src/types/book.ts
@@ -0,0 +1,8 @@
+export type Book = {
+  id: string;
+  isbn?: string;
+  title?: string;
+  author?: string;
+  image_url?: string;
+  link?: string;
+};
