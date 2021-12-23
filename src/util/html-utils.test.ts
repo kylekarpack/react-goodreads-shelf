38f539d6c010f6bc67d4e04ff589c39cb290e49c
Kyle Karpack <kylekarpack@gmail.com>
2021-12-23 13:07:51 -0800

    Add HTML utils tests

---
+++ src/util/html-utils.test.ts
@@ -0,0 +1,18 @@
+import { getBooksFromHtml } from "./html-utils";
+import fs from "fs";
+import path from "path";
+
+const filePath = path.join(__dirname, "./response.test.html");
+const sampleHtml = fs.readFileSync(filePath).toString();
+
+describe("html utilities", () => {
+  it("returns books from empty html", () => {
+    const books = getBooksFromHtml("");
+    expect(books.length).toBe(0);
+  });
+
+  it("returns books from sample html", () => {
+    const books = getBooksFromHtml(sampleHtml);
+    expect(books.length).toBe(5);
+  });
+});
