41f4d74e2988afbe60eea15231fb340cfe31d8e9
Kyle Karpack <kylekarpack@gmail.com>
2021-12-23 17:12:33 -0800

    Update setup files

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   automock: false,
   coverageDirectory: "coverage",
   collectCoverageFrom: ["**/*.{ts,tsx}", "!**/*.d.ts", "!**/src/index.ts", "!**/src/types/**", "!**/node_modules/**"],
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
   testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
   transform: {
     "^.+\\.(t|j)sx?$": [
@@ -13,7 +13,7 @@ module.exports = {
       {
         sourceMaps: true,
         jsc: {
-          target: "es2021",
+          target: "es2022",
           transform: {
             react: {
               runtime: "automatic"
