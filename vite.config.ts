981504121a452bc38d97e02a116d8054f69e4aa7
Kyle Karpack <kylekarpack@gmail.com>
2022-01-26 13:56:05 -0800

    Add vite

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+
+export default defineConfig({
+  plugins: [react(), dts()],
+  build: {
+    outDir: "./dist",
+    lib: {
+      entry: "./index.ts",
+      formats: ["cjs", "es"],
+      fileName: "index"
+    },
+    minify: "esbuild",
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ["react"],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: "React"
+        },
+        exports: "named"
+      }
+    }
+  }
+});
