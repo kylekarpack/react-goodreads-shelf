b0941bf02f8a3357017a1c0926736cc95344d3d9
Kyle Karpack <kylekarpack@gmail.com>
2022-01-27 11:49:05 -0800

    Try updating storybook

--- stories/0-GoodreadsBookshelf.stories.tsx
+++ stories/0-GoodreadsBookshelf.stories.tsx
@@ -1,7 +1,7 @@
 import type { ComponentMeta } from "@storybook/react";
 import React from "react";
-import GoodreadsBookshelf from "../dist/index";
-import type { Props } from "../dist/types";
+import GoodreadsBookshelf from "../dist/index.cjs";
+import type { Props } from "../dist/src/custom";
 
 const sorts = [
   "title",
