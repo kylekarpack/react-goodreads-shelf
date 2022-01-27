868e50cf839e2926059147e8aa407d4d75c1389f
Kyle Karpack <karpack@adobe.com>
2022-01-27 13:37:25 -0800

    Improve props typing

--- src/types/props.ts
+++ src/types/props.ts
@@ -1,12 +1,45 @@
+type SortKey =
+  | "title"
+  | "author"
+  | "cover"
+  | "rating"
+  | "year_pub"
+  | "date_pub"
+  | "date_pub_edition"
+  | "date_started"
+  | "date_read"
+  | "date_updated"
+  | "date_added"
+  | "recommender"
+  | "avg_rating"
+  | "num_ratings"
+  | "review"
+  | "read_count"
+  | "votes"
+  | "random"
+  | "comments"
+  | "notes"
+  | "isbn"
+  | "isbn13"
+  | "asin"
+  | "num_pages"
+  | "format"
+  | "position"
+  | "shelves"
+  | "owned"
+  | "date_purchased"
+  | "purchase_location"
+  | "condition";
+
 export type Props = {
   /** The user ID for whom to fetch books */
   userId: string;
   /** The shelf from which to fetch books */
-  shelf?: string;
+  shelf?: "read" | "currently-reading" | "to-read" | string;
   /** Minimum width allowed for each book */
   width?: number;
   /** The field by which to sort the results returned */
-  sort?: string;
+  sort?: SortKey;
   /** The direction in which to sort the results */
   order?: "a" | "d";
   /** The maximum number of books to be returned */
