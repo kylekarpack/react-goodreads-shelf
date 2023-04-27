1f4bd1031d33456c46dba9d9992e14353566ee52
Kyle Karpack <kylekarpack@gmail.com>
2023-04-26 22:02:55 -0700

    Storybook fixes

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -3,12 +3,11 @@ import { themes } from "@storybook/theming";
 const parameters = {
   darkMode: {
     // Override the default dark theme
-    dark: { ...themes.dark, appBg: "black", color: "white" },
+    dark: { ...themes.dark, appBg: "black", appContentBg: "black", color: "white", base: "dark" },
     // Override the default light theme
     light: { ...themes.normal, appBg: "white" },
     darkClass: "lights-out",
-    lightClass: "lights-on",
-    stylePreview: true
+    lightClass: "lights-on"
   }
 };
 
