0bf74039bba14fde13a83d21686cf5f08a0e43cf
Kyle Karpack <kylekarpack@gmail.com>
2023-04-26 20:01:16 -0700

    Dark mode fixes

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,27 +1,20 @@
 import { themes } from "@storybook/theming";
-import { useDarkMode } from "storybook-dark-mode";
-import React, { FunctionComponent } from "react";
+import "./preview.css";
 
 const parameters = {
   darkMode: {
     // Override the default dark theme
     dark: { ...themes.dark, appBg: "black", color: "white" },
     // Override the default light theme
-    light: { ...themes.normal, appBg: "white" }
+    light: { ...themes.normal, appBg: "white" },
+    darkClass: "lights-out",
+    lightClass: "lights-on",
+    stylePreview: true
   }
 };
 
-// create a component that uses the dark mode hook
-const ThemeWrapper: FunctionComponent<any> = ({ children }) => {
-  // render your custom theme provider
-  return <div style={{ color: useDarkMode() ? "white" : "black", fontFamily: "sans-serif" }}>{children}</div>;
-};
-
-const decorators = [(renderStory) => <ThemeWrapper>{renderStory()}</ThemeWrapper>];
-
 const preview = {
-  parameters,
-  decorators
+  parameters
 };
 
 export default preview;
