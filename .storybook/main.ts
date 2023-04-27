13af3c8e597ae5abb011a817751373b06be53181
Kyle Karpack <kylekarpack@gmail.com>
2023-04-26 17:55:24 -0700

    Update Storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,11 +1,8 @@
 import type { StorybookViteConfig } from "@storybook/builder-vite";
-
 const config: StorybookViteConfig = {
   stories: ["../stories/**/*.stories.{ts,tsx}"],
-  addons: ["@storybook/addon-controls", "storybook-dark-mode"],
-  core: {
-    builder: "@storybook/builder-vite"
-  },
+  addons: ["@storybook/addon-controls", "storybook-dark-mode", "@storybook/addon-mdx-gfm"],
+  core: {},
   typescript: {
     check: false,
     checkOptions: {}
@@ -13,7 +10,13 @@ const config: StorybookViteConfig = {
   async viteFinal(config) {
     config.base = "/react-goodreads-shelf/";
     return config;
+  },
+  framework: {
+    name: "@storybook/react-vite",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
 };
-
 export default config;
